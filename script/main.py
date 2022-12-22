747e5a4ada21684bc61d9e5f4f15f8d42ddc8ec5
Nater0214 <gamer.nater0214@gmail.com>
2022-12-22 17:50:11 -0500

    Initial commit

---
+++ script/main.py
@@ -0,0 +1,13 @@
+# main.py
+# Py Grader
+# Makes it easy to grade an assignment based on test cases
+
+
+# Definitions
+def main() -> None:
+    pass
+
+
+# Run
+if __name__ == "__main__":
+    main()
